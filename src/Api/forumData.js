aceeaa780ec2cf7395b5376359d60a5feb7230b2
Justin Sun <justinsunyt@gmail.com>
2020-03-14 22:48:36 -0700

    added elements to forum post

--- src/Api/forumData.js
+++ src/Api/forumData.js
@@ -4,35 +4,54 @@ const forumData = [
         likes: 10,
         title: "Unit 10 Study Guide",
         text: "https://google.com",
-        date: new Date(2020, 3, 14)
+        date: new Date(2020, 3, 14),
+        creator: "Josh",
+        class: "Chemistry Honors",
+        comments: {
+            "Justin": "Thanks!"
+        }
     },
     {
         id: 2,
         likes: 69,
         title: "Book PDF",
         text: "Don't buy the book from the bookstore! It's here for free...",
-        date: new Date(2020, 3, 10)
+        date: new Date(2020, 3, 10),
+        creator: "Anonymous",
+        class: "English II",
+        comments: {
+            "Josh": "Thanks for the tip!"
+        }
     },
     {
         id: 3,
-        likes: 3,
+        likes: 1,
         title: "Confusing Chem Lab",
         text: "I don't get this Thermodynamics thing at all. Can someone help?",
-        date: new Date(2020, 3, 12)
+        date: new Date(2020, 3, 12),
+        creator: "Anonymous",
+        class:"Chemistry Honors",
+        comments: {}
     },
     {
         id: 4,
         likes: 20,
         title: "Class Name Suggestion",
         text: "Please comment some class name suggestions for class next week!",
-        date: new Date(2020, 3, 9)
+        date: new Date(2020, 3, 9),
+        creator: "Hope",
+        class:"Precalculus Honors",
+        comments: {}
     },
     {
         id: 5,
         likes: 8,
         title: "Video on What We Learned in Class",
         text: "https://youtube.com",
-        date: new Date(2020, 3, 14)
+        date: new Date(2020, 3, 14),
+        creator: "Anonymous",
+        class:"World and Europe II",
+        comments: {}
     }
 ]
 
--- src/Components/ForumPost.js
+++ src/Components/ForumPost.js
@@ -7,20 +7,28 @@ function ForumPost(props) {
     const year = props.post.date.getFullYear()
     const month = props.post.date.getMonth()
     const day = props.post.date.getDate()
+    const creator = props.post.creator
+    const numComments = Object.keys(props.post.comments).length
+    const className = props.post.class
 
     return (
         <div className="forum-post">
-            <div className="forum-title">
+            <div className="forum-header">
+                <p align="left">from {className}</p>
+                <p align="right"><b>{likes} {(likes == 1) ? "like" : "likes"}</b></p>
+            </div>  
+            <div className="forum-title">   
                 <h2 className="forum-title">{title}</h2>
-                <p align="right"><b>{likes} likes</b></p>
             </div>
             
             <div className="forum-text">
                 <p>{text}</p>
             </div> 
-            <div className="forum-date">
-                <p><i>{month} / {day} / {year}</i></p>
-            </div>  
+            <div className="forum-footer">
+                <p>Posted by <i>{creator} - {month} / {day} / {year}</i></p>
+                <p>{numComments} {(numComments == 1) ? "comment" : "comments"}</p>
+            </div>
+            <hr />  
         </div>
     )
 }
