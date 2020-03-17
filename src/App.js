d3b38e2957fd4f72a7d6c960c782702b479c6d12
Justin Sun <justinsunyt@gmail.com>
2020-03-17 14:58:35 -0700

    react-router, individual posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Forum from './Components/Forum'
+import ForumDetail from './Components/ForumDetail'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Forum />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={Forum}/>
+          <Route path="/post/:id" component={ForumDetail}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
