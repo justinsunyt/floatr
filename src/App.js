b3e03b6381c9a50a433a9595731f008547258dc1
Joshua Cheng <joshua-admin@Poco-Coco.local>
2020-03-21 13:31:54 -0700

    added loging in with a google account

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Forum from './Components/Forum'
 import ForumDetail from './Components/ForumDetail'
 import ClassDetail from './Components/ClassDetail'
 import Nav from './Components/Nav'
+import Homepage from './Components/HomepageLogin'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
         <Nav />
         <br />
         <Switch>
-          <Route path="/" exact render={() => <Forum filter={"all"}/>} />
+          
+          <Route path="/" exact component={Homepage}/>} 
           <Route path="/post/:id" component={ForumDetail}/>
           <Route path="/class/:id" component={ClassDetail}/>
         </Switch>
@@ -23,3 +25,8 @@ function App() {
 }
 
 export default App;
+const Home = () => (
+  <div>
+  <h1>Welcome to TaskFloat</h1>
+</div>
+)
---
+++ src/Components/HomepageLogin.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import firebase from 'firebase'
+
+
+function Homepage()
+{
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    return(
+        <div>
+            
+            <center>
+            <h1>Welcome to TaskFloat</h1>
+            <p>Please Login with your Google Account to continue</p>
+            <button onClick={
+            function()
+            {
+                firebase.auth().signInWithPopup(provider).then(function(result) {
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    var token = result.credential.accessToken;
+                    // The signed-in user info.
+                    var user = result.user;
+                    // then stuff
+                    console.log(user);
+
+                  }).catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    // ...
+                  });
+            }
+
+            }>Login</button>
+            </center>
+            <body>
+
+            
+            </body>
+        </div>
+      
+    )
+       
+}
+export default Homepage
