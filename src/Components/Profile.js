b1343059654effd3d36e98691f7182216dd04783
Justin Sun <justinsunyt@gmail.com>
2020-03-21 19:10:43 -0700

    classes and profile page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -6,11 +6,18 @@ function Profile() {
     const {currentUser} = useContext(AuthContext)
     console.log(currentUser)
     const displayName = currentUser.displayName
+    const profilePic = currentUser.photoURL
+
     return(
-        <div className="profile-header">
-            <h1>Profile</h1>
-            <button onClick={() => firebase.auth().signOut()}>Sign out</button>
-            <p>{displayName}</p>
+        <div>
+            <div className="profile-header">
+                <h1>Profile</h1>
+                <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+            </div>
+            <div className="profile-details">
+                <img src={profilePic} className={"profile-pic"}/>
+                <h3>{displayName}</h3>
+            </div> 
         </div>
     )
 }
