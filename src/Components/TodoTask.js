6e85e9ec984566657e5898a3fec7161cfd714575
Justin Sun <justinsunyt@gmail.com>
2020-04-21 22:01:42 -0700

    Changed show menu to left click

--- src/Components/TodoTask.js
+++ src/Components/TodoTask.js
@@ -26,10 +26,17 @@ function TodoTask(props) {
         color: "red"
     }
 
+    function toggleMenu(event) {
+        if (event.target.id !== "date" && event.target.id !== "form" && event.target.id !== "menu") {
+            setShowMenu(!showMenu)
+        }
+    }
+
     return (
-        <div className="todo-task" onClick={() => setShowMenu(!showMenu)}>
+        <div className="todo-task" onClick={toggleMenu}>
             <div className="todo-title">
                 <input 
+                    id="form"
                     type="checkbox" 
                     checked={completed} 
                     onChange={() => props.handleChange(id, "task")}
@@ -42,8 +49,8 @@ function TodoTask(props) {
             </div>
             <hr/>
             <CSSTransition in={showMenu} timeout={300} classNames="menu" unmountOnExit>
-                <div className="todo-menu" id={"menu" + id}>
-                    <div className='todo-form'>
+                <div className="todo-menu" id="menu">
+                    <div className='todo-form' id="form">
                         <form onSubmit={event => {
                             event.preventDefault()
                             props.handleChange(id, "date")
@@ -51,7 +58,7 @@ function TodoTask(props) {
                         }}>
                             <input type="text" name="date" id="date" autocomplete="off" required/>
                             <label for="date" class="task-label">
-                                <span class="task-content">Set new date: M/D(/Y)</span>
+                                <span class="task-content">Set new date: Month/Day</span>
                             </label>
                         </form>
                     </div>
