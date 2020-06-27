f741f1c6d0df3e9d1fad05f69d5959e7a885023e
Justin Sun <justinsunyt@gmail.com>
2020-06-26 18:07:04 -0700

    JoinClass Search

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const algoliasearch = require('algoliasearch')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const ALGOLIA_ID = functions.config().algolia.app_id
+const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key
+
+const ALGOLIA_INDEX_NAME = process.env.GCLOUD_PROJECT === 'floatrrr' ? 'prod_classes' : 'dev_classes'
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY)
+const index = client.initIndex(ALGOLIA_INDEX_NAME)
+
+exports.onClassWrite = functions.firestore.document('classes/{classId}').onWrite((change, context) => {
+    if (!change.after.exists) {
+        return index.deleteObject(context.params.classId)
+    } else {
+        const cl = change.after.data()
+        cl.objectID = context.params.classId
+        return index.saveObject(cl)
+    }
+})
